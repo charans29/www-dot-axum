2f603e373bf0b4cb064c91b5b4404f9b19247134
charans29 <smodugula@csuchico.edu>
2024-09-24 21:12:18 -0400

    added page@docs

--- src/components/About.rs
+++ src/components/About.rs
@@ -1,4 +1,5 @@
 use leptos::{component, view, IntoView};
+use crate::components::docs::DocsPage;
 
 #[component]
 pub fn AboutPage() -> impl IntoView {
@@ -7,5 +8,8 @@ pub fn AboutPage() -> impl IntoView {
             <h1>"About ME"</h1>
             <p>"This is the about page."</p>
         </div>
+        <section id="about" class="h-screen">
+            <DocsPage/>
+        </section>
     }
 }
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,2 +1,3 @@
 pub mod home;
 pub mod about;
+pub mod docs;
